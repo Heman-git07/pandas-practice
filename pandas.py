6941013342e4249ad9c17b47523337fbd89040de
default <heman28102002@gmail.com>
2024-09-03 10:41:26 +0000

    task3

--- pandas.py
+++ pandas.py
@@ -2,3 +2,17 @@
 """pip install pandas"""
 #Once Pandas is installed, import it in your applications by adding the import keyword
 """import pandas"""
+
+#pandas series
+#1
+import pandas as pd
+data=["heman","muthu","saro"]
+data_1=pd.Series(data)
+print(data_1)
+#2we can also select particular value by using index
+print(data_1[0])
+#3cwe can also set our own index using index word
+
+data_2=["apple","orange","pineapple"]
+data_3=pd.Series(data_2,index=["a","b","c"])
+print(data_3["c"])
