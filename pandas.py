05ff282afb77e72407d7e429dde58e9ff7a7a864
default <heman28102002@gmail.com>
2024-09-06 07:50:39 +0000

    task15

--- pandas.py
+++ pandas.py
@@ -121,5 +121,13 @@ import pandas as pd
 df=pd.read_csv("csv file")
 print(pd.info())
 
+#Return a new Data Frame with no empty cells:
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+new_df = df.dropna()
+
+print(new_df.to_string())
 
 
