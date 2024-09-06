419793573c5acedbdab80a8a91aa7d58c775d0e1
default <heman28102002@gmail.com>
2024-09-06 07:59:57 +0000

    task20

--- pandas.py
+++ pandas.py
@@ -192,6 +192,21 @@ df['Date'] = pd.to_datetime(df['Date'])
 
 print(df.to_string())
 
+#Remove rows with a NULL value in the "Date" column:
+df.dropna(subset=['Date'], inplace = True)
+
+#Replacing Values
+#Set "Duration" = 45 in row 7:
+df.loc[7, 'Duration'] = 45
+
+#Loop through all values in the "Duration" column.If the value is higher than 120, set it to 120:
+for x in df.index:
+  if df.loc[x, "Duration"] > 120:
+    df.loc[x, "Duration"] = 120
+#Delete rows where "Duration" is higher than 120:
+for x in df.index:
+  if df.loc[x, "Duration"] > 120:
+    df.drop(x, inplace = True)
 
 
 
