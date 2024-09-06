3972d296d770a81ad76223c8ffc20084396df779
default <heman28102002@gmail.com>
2024-09-06 07:52:36 +0000

    task16

--- pandas.py
+++ pandas.py
@@ -130,4 +130,21 @@ new_df = df.dropna()
 
 print(new_df.to_string())
 
+#Remove all rows with NULL values:
+
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+df.dropna(inplace = True)
+
+print(df.to_string())
+
+#Replace NULL values with the number 130:
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+df.fillna(130, inplace = True)
+
 
